30e6122e2cbf106778e085e64d90ba6ff22dcb62
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    sirius: Exclude mokee-sdk overlays from RRO
    * mokee-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- sirius.mk
+++ sirius.mk
@@ -30,6 +30,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-mokee/mokee-sdk
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
